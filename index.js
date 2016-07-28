e019705049ee8c8e7cd38e09e4ddec7a4a05f59b
Anna Warzecha <anna.warzecha@gmail.com>
2016-07-28 18:50:38 +0200

    Add label as a new syntax for setting the label

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+function label(blk) {
+    var body = blk.body.replace(/\\/g, "")
+    return '<p class="code-label">' + body + '</p>';
+}
+
 module.exports = {
     book: {
         assets: "./book",
@@ -5,10 +10,10 @@ module.exports = {
     },
     blocks: {
         filename: {
-            process: function(blk) {
-                var body = blk.body.replace(/\\/g, "")
-                return '<p class="code-filename">' + body + '</p>';
-            }
+            process: label
+        },
+        label: {
+            process: label
         }
     }
 };
